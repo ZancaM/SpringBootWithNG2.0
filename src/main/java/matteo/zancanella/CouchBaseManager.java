4c1afe3221a3d25c96b9d354cebe440e93890ba2
Matteo Zancanella <matteo.zancanella@ge.com>
2017-01-13 18:04:46 -0500

    Fixed initial commit

---
+++ src/main/java/matteo/zancanella/CouchBaseManager.java
@@ -0,0 +1,10 @@
+package matteo.zancanella;
+
+import com.couchbase.client.java.*;
+import com.couchbase.client.java.document.*;
+import com.couchbase.client.java.document.json.*;
+import com.couchbase.client.java.query.*;
+
+public class CouchBaseManager {
+
+}
