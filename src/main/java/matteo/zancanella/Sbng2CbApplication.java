b926a955a54038c63760ebde4a34d317cf74e42d
Matteo Zancanella <matteo.zancanella@ge.com>
2017-01-13 17:49:40 -0500

    first commit

---
+++ src/main/java/matteo/zancanella/Sbng2CbApplication.java
@@ -0,0 +1,12 @@
+package matteo.zancanella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbng2CbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbng2CbApplication.class, args);
+	}
+}
---
+++ src/test/java/matteo/zancanella/Sbng2CbApplicationTests.java
@@ -0,0 +1,16 @@
+package matteo.zancanella;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Sbng2CbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
